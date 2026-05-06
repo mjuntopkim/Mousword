99ab94ac55c28d7af573fcbe412c28e0ab7a6d46
wjswodnjs <jym643854@gmail.com>
2026-05-06 15:41:02 +0900

    player_move_improve

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,7 +3,10 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float moveSpeed = 5f;    
-    public float jumpForce = 7f;    
+    public float jumpForce = 7f;
+
+    public float fallMultiplier = 2.5f; // 떨어질 때 추가로 받을 중력 배수
+    public float lowJumpMultiplier = 2f;    //점프기를 짧게 눌렀을 때 받는 중력
 
     public bool isGrounded;            // 현재 바닥인지 확인
     public Transform groundCheck;      // 발밑 위치를 정할 오브젝트
@@ -29,6 +32,19 @@ public class PlayerMove : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rigid.linearVelocity = new Vector2(rigid.linearVelocity.x, jumpForce);
+        }
+
+        // 4. 떨어질 때 묵직하게 (기존에 추가했던 코드)
+        if (rigid.linearVelocity.y < 0)
+        {
+            rigid.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        // 5. 짧은 점프 처리 (새로 추가할 마법의 코드!)
+        // 캐릭터가 위로 올라가는 중(y > 0) 인데, 점프 버튼(Jump)에서 손을 뗐다면(!Input.GetButton)
+        else if (rigid.linearVelocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            // 중력을 강하게 줘서 상승을 확 꺾어버립니다.
+            rigid.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
 

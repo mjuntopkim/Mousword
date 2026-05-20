2d0876180e763998ce37faa7cd8102ad244957ad
wjswodnjs <jym643854@gmail.com>
2026-05-20 17:42:06 +0900

    character_design

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -16,9 +16,15 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D rigid;
     private float moveInput;
 
+    private Animator anim;                 // 애니메이션 조종기
+    private SpriteRenderer spriteRenderer; // 좌우 방향 뒤집기용
+
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
+
+        anim = GetComponent<Animator>();                 
+        spriteRenderer = GetComponent<SpriteRenderer>(); 
     }
 
 
@@ -34,18 +40,33 @@ public class PlayerMove : MonoBehaviour
             rigid.linearVelocity = new Vector2(rigid.linearVelocity.x, jumpForce);
         }
 
-        // 4. 떨어질 때 묵직하게 (기존에 추가했던 코드)
+        // 4. 떨어질 때 묵직하게
         if (rigid.linearVelocity.y < 0)
         {
             rigid.linearVelocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        // 5. 짧은 점프 처리 (새로 추가할 마법의 코드!)
+        // 5. 짧은 점프 처리 
         // 캐릭터가 위로 올라가는 중(y > 0) 인데, 점프 버튼(Jump)에서 손을 뗐다면(!Input.GetButton)
         else if (rigid.linearVelocity.y > 0 && !Input.GetButton("Jump"))
         {
-            // 중력을 강하게 줘서 상승을 확 꺾어버립니다.
+            // 중력을 강하게 줘서 상승을 확 꺾어버린다.
             rigid.linearVelocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
+
+        // 캐릭터 좌우 뒤집기
+        if (moveInput > 0)
+        {
+            spriteRenderer.flipX = false; // 오른쪽을 볼 때 (기본)
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;  // 왼쪽을 볼 때 (반전)
+        }
+
+        // 애니메이터에게 현재 속도(절댓값) 넘겨주기
+        anim.SetFloat("Speed", Mathf.Abs(moveInput));
+
+        anim.SetBool("isGrounded", isGrounded);
     }
 
     void FixedUpdate()

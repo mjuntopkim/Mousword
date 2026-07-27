c33c719d692add4df24b9b3d8d0dbaa1b37e8c49
mjuntopkim <mjuntopkim2893@gmail.com>
2026-07-27 15:28:24 +0900

    Fix: AutoTargetPlayer.cs의 무한루프 제거
    - AutoTargetPlayer.cs의 코루틴에서 플레이어를 찾는 부분이 while로 되어있었으나 플레이어가 없는 경우 무한 루프를 빠져나올 수 없어 탐색 횟수를 10회 부여하고 10회를 넘어가면 반복문 탈출하도록 설정

--- Assets/Scripts/Camera/AutoTargetPlayer.cs
+++ Assets/Scripts/Camera/AutoTargetPlayer.cs
@@ -12,15 +12,23 @@ public class AutoTargetPlayer : MonoBehaviour
     private IEnumerator FindFollowPlayerRoutine()
     {
         GameObject player = null;
-        while(player == null)
+        int maxFind = 10;
+        int currentFind = 0;
+
+        while(player == null && currentFind < maxFind)
         {
             player = GameObject.FindWithTag("Player");
+            currentFind++;
             yield return null;
         }
 
-        if(TryGetComponent<CinemachineCamera>(out var vcam))
+        if(player != null && TryGetComponent<CinemachineCamera>(out var vcam))
         {
             vcam.Follow = player.transform;
+        }
+        else if(player == null)
+        {
+            Debug.Log("플레이어를 찾지 못함");
         }
     }
 }
